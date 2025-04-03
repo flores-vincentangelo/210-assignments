0579168d8ae42ad99f08da070b0a2040c4e59d9b
Vincent <vincentflores88@gmail.com>
2025-04-03 13:23:47 +0800

    updates to pearsons

--- pearsons.py
+++ pearsons.py
@@ -6,8 +6,13 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from dataETL.dataModel import Respondents
 from scipy import stats
+from tabulate import tabulate
 
-engine = create_engine(f"sqlite:///./{os.environ["DB_NAME"]}", echo=True)
+engine = create_engine(f"sqlite:///./{os.environ["DB_NAME"]}", echo=False)
+
+analysis_path = "analysis/pearsons"
+if not os.path.exists(analysis_path):
+        os.makedirs(analysis_path)
 
 def get_data(engine):
 
@@ -27,8 +32,7 @@ def get_data(engine):
             numerical_attribute_dict["budget_eat_out"].append(respondent.budget_eat_out)
             numerical_attribute_dict["budget_takeout_delivery"].append(respondent.budget_takeout_delivery)
             numerical_attribute_dict["grocery_cost"].append(respondent.grocery_cost)
-    data_dict = numerical_attribute_dict.copy()
-    return data_dict
+    return numerical_attribute_dict
 
 def make_scatter_plots(x_label, y_label, x, y):
     figure_path = "figures/pearsons-scatter"
@@ -42,7 +46,7 @@ def make_scatter_plots(x_label, y_label, x, y):
     plt.ylabel(y_label)
     ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
     plt.savefig(f"{figure_path}/scatter-{x_label}-{y_label}.png")
-    plt.clf()
+    plt.close()
 
 
 def make_histogram(numerical_attribute_dict, attribute):
@@ -53,11 +57,11 @@ def make_histogram(numerical_attribute_dict, attribute):
     plt.ylabel("frequency")
     plt.hist(numerical_attribute_dict[attribute])
     plt.savefig(f"{figure_path}/histogram-{attribute}.png")
-    plt.clf()
+    plt.close()
     # if attribute == "budget_eat_out":
         # plt.show()
 
-def pearsons(data_dict):
+def pearsons_correlation(data_dict):
     numerical_attribute_dict = data_dict
     pearsons_dict = {}
     pearsons_list = []
@@ -84,33 +88,45 @@ def pearsons(data_dict):
 
     return pearsons_list
 
+def pretty_print(pearsons_list, to_csv=False):
+    f = open(f"{analysis_path}/pearsons_list.csv", "w") if to_csv else None
+    table = []
+    table.append(["Attribute 1","Attribute 2","Correlation Coefficient","Rank"])
+    if to_csv:
+        f.write(f"Attribute 1,Attribute 2,Correlation Coefficient,Rank\n")
+    count = 1
+    for obj in pearsons_list:
+        attr1 = obj["attributes"][0]
+        attr2 = obj["attributes"][1]
+        if to_csv:
+            f.write(f"{attr1["name"]},{attr2["name"]},{obj["statistic"]},{count}\n")
+        table.append([attr1["name"],attr2["name"],obj["statistic"],count])
+        count += 1
+    print(tabulate(table, headers="firstrow", tablefmt="fancy_grid"))
+    if to_csv:
+        f.close()
 
+numerical_data_dict = get_data(engine)
+pearsons_list = pearsons_correlation(numerical_data_dict)
 
-data_dict = get_data(engine)
-pearsons_list = pearsons(data_dict)
-
-for attr in data_dict.keys():
-    make_histogram(data_dict, attr)
+for attr in numerical_data_dict.keys():
+    make_histogram(numerical_data_dict, attr)
 for obj in pearsons_list:
     attr1 = obj["attributes"][0]
     attr2 = obj["attributes"][1]
     make_scatter_plots(attr1["name"], attr2["name"], attr1["values"], attr2["values"])
 
-def sorting_function(obj):
-    return obj["statistic"]
-pearsons_list.sort(key=sorting_function, reverse=True)
 
-analysis_path = "analysis/pearsons"
-if not os.path.exists(analysis_path):
-        os.makedirs(analysis_path)
 
 with open(f"{analysis_path}/attr_pearsons_list.json", "w") as f:
     f.write(json.dumps(pearsons_list))
-with open(f"{analysis_path}/pearsons_list.csv", "w") as f:
-    f.write(f"Attribute 1,Attribute 2,Correlation Coefficient,Rank\n")
-    count = 1
-    for obj in pearsons_list:
-        attr1 = obj["attributes"][0]
-        attr2 = obj["attributes"][1]
-        f.write(f"{attr1["name"]},{attr2["name"]},{obj["statistic"]},{count}\n")
-        count += 1
+# with open(f"{analysis_path}/pearsons_list.csv", "w") as f:
+#     f.write(f"Attribute 1,Attribute 2,Correlation Coefficient,Rank\n")
+#     count = 1
+#     for obj in pearsons_list:
+#         attr1 = obj["attributes"][0]
+#         attr2 = obj["attributes"][1]
+#         f.write(f"{attr1["name"]},{attr2["name"]},{obj["statistic"]},{count}\n")
+#         count += 1
+
+pretty_print(pearsons_list)
