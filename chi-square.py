229f7b1ec676ccadb8af14ae1624978ea5682881
Vincent <vincentflores88@gmail.com>
2025-04-03 15:01:39 +0800

    cleaning and commenting

--- chi-square.py
+++ chi-square.py
@@ -1,48 +1,78 @@
 import json
 import os
-from sqlalchemy import create_engine, func, inspect, select, text
+import numpy as np
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine, func, inspect, select
 from sqlalchemy.orm import Session
 from dataETL.dataModel import Respondents
-import numpy as np
 from scipy.stats import chi2_contingency
 
-engine = create_engine(f"sqlite:///./{os.environ["DB_NAME"]}", echo=False)
+engine = create_engine(f"sqlite:///./{os.environ["DB_NAME"]}", echo=True)
 
 categorical_attributes_list = [
     "gender",
     "relationship_status",
     "employment_status",
-    # "monthly_income",
     "pet_ownership",
     "housing_type",
     "primary_cook",
     "preferred_dining",
-    # "health_consciousness",
-    # "frequency_eating_out",
-    # "frequency_takeout_delivery",
-    # "frequency_cook_home",
-    # "frequency_grocery",
 ]
 
-def prepare_attr_mapping(engine, categorical_attributes_list):
+def get_cat_attr_combinations(engine, categorical_attributes_list):
 
+    # This method assembles the dictionary for all combinations of categorical attributes
+    # as well as the set of all categorical values used in each of the columns.
+    # The method returns a dictionary of all categorical attributes combinations. 
+    # An example using "gender" and "relationship_status" is shown below:
+    """ 
+    {
+        "gender": {
+            "relationship_status": {
+                "Female": {
+                    "Married": 0,
+                    "Married but not living together": 0,
+                    "Single": 0
+                },
+                "Male": {
+                    "Married": 0,
+                    "Married but not living together": 0,
+                    "Single": 0
+                }
+            },
+            "employment_status": {...},
+            "pet_ownership": {...},
+            ...
+        },
+        "relationship_status": {...},
+        ...  
+    }
+     """
     categorical_attr_mappings = {}
-    column_attr_list = []
     column_attr_values_dict = {}
+
+    # get metadata of "Respondents" table
     mapper = inspect(Respondents)
+
+    # get all unique categorical value found for each attribute/column of the table
+    # this is done by selecting the columnn and grouping by itself, therefore getting all unique values.
+    # "mapper.attrs" gets all column entities of the "Respondents" table
     for column_attr in mapper.attrs:
         if column_attr.key not in categorical_attributes_list:
             continue
-        column_attr_list.append(column_attr.key)
+
         column_attr_values_dict[column_attr.key] = []
+
         with Session(engine) as session:
-            group_by = select(column_attr, func.count(column_attr).label("counts")).group_by(column_attr)
-            for result_tuple in session.execute(group_by):
+            # grouping query
+            query = select(column_attr).group_by(column_attr)
+            for result_tuple in session.execute(query):
                 column_attr_values_dict[column_attr.key].append(result_tuple[0])
     
-    for attr1 in column_attr_list:
+    # assembly of categorical attribute dictionary
+    for attr1 in column_attr_values_dict.keys():
         categorical_attr_mappings[attr1] = {}
-        for attr2 in column_attr_list:
+        for attr2 in column_attr_values_dict.keys():
             if attr1 == attr2:
                 continue
             if attr2 in categorical_attr_mappings:
@@ -57,7 +87,8 @@ def prepare_attr_mapping(engine, categorical_attributes_list):
 def get_totals(engine, categorical_attributes_list, categorical_attr_mappings):
     
     mapper = inspect(Respondents)
-    filepath = "categorical_attr_mappings.txt"
+    chi_square_filepath = "analysis/chi_square"
+    filepath = f"{chi_square_filepath}/categorical_attr_mappings.txt"
     if os.path.exists(filepath):
         os.remove(filepath)
     with open(filepath, "a") as f:
@@ -199,7 +230,10 @@ def chi_square(array):
     return tuple((
         res.statistic,
         res.dof,
-        null_hypothesis
+        critical_value_dict[res.dof],
+        null_hypothesis,
+        obs,
+        res.expected_freq
     ))
 
 def chi_square_analysis(cat_att_maps):
@@ -211,6 +245,8 @@ def chi_square_analysis(cat_att_maps):
         chi_square_dict[attr1] = {}
         for attr2 in cat_att_maps[attr1].keys():
             chi_square_list = []
+
+            # looping through values
             for attr1_values in cat_att_maps[attr1][attr2].keys():
                 value2_list = []
                 for attr2_values in cat_att_maps[attr1][attr2][attr1_values].keys():
@@ -218,7 +254,7 @@ def chi_square_analysis(cat_att_maps):
                 
                 chi_square_list.append(value2_list)
             
-            (statistic, dof, null_hypothesis) = chi_square(chi_square_list)
+            (statistic, dof, critical_value, null_hypothesis, obs, expected_freq) = chi_square(chi_square_list)
             chi_square_dict[attr1][attr2] = {
                 "statistic": statistic,
                 "dof": dof,
@@ -228,8 +264,13 @@ def chi_square_analysis(cat_att_maps):
             attr_chi_square_list.append({
                 "attributes": [attr1, attr2],
                 "dof": dof,
+                "critical_value": critical_value,
                 "statistic": round(statistic, 5),
-                "null_hypothesis": null_hypothesis
+                "null_hypothesis": null_hypothesis,
+                "x_axis":list(cat_att_maps[attr1][attr2][attr1_values].keys()),
+                "y_axis":list(cat_att_maps[attr1][attr2].keys()),
+                "obs": obs.tolist(),
+                "expected": expected_freq.tolist()
             })
             if (null_hypothesis == "Reject. Values related"):
                 print(f"{attr1} {attr2} are related!")
@@ -237,18 +278,91 @@ def chi_square_analysis(cat_att_maps):
     return (chi_square_dict, attr_chi_square_list)
 
         
-            
+chi_square_filepath = "analysis/chi_square"
+figure_path = "figures/chi_square"
+if not os.path.exists(chi_square_filepath):
+    os.makedirs(chi_square_filepath)
+if not os.path.exists(figure_path):
+        os.makedirs(figure_path)   
 
-categorical_attr_mappings = prepare_attr_mapping(engine, categorical_attributes_list)
+categorical_attr_mappings = get_cat_attr_combinations(engine, categorical_attributes_list)
 categorical_attr_mappings = get_totals(engine, categorical_attributes_list, categorical_attr_mappings)
-with open("categorical_attr.json", "w") as f:
+
+with open(f"{chi_square_filepath}/categorical_attr_mappings.json", "w") as f:
     f.write(json.dumps(categorical_attr_mappings))
+
 (chi_square_dict, chi_square_list) = chi_square_analysis(categorical_attr_mappings)
-with open("chi_square_dict.json", "w") as f:
-    f.write(json.dumps(chi_square_dict))
+
+# with open(f"{chi_square_filepath}/chi_square_dict.json", "w") as f:
+#     f.write(json.dumps(chi_square_dict))
+
 def sorting_function(obj):
     return obj["statistic"]
 chi_square_list.sort(key=sorting_function)
-with open("attr_chi_square_list.json", "w") as f:
+with open(f"{chi_square_filepath}/chi_square_list.json", "w") as f:
     f.write(json.dumps(chi_square_list))
+
+for obj in chi_square_list:
+    attr1 = obj["attributes"][0]
+    attr2 = obj["attributes"][1]
+    row_count = len(obj["y_axis"])
+    column_count = len(obj["x_axis"])
+    header = f"Attributes:,{attr1},{attr2}\nDegree(s) of freedom:,{obj["dof"]}\nCritical value:,{obj["critical_value"]}\nStatistic:,{obj["statistic"]}\n"
+
+    value_comb_list = []
+    observed_list = []
+    expected_list = []
+
+    with open(f"{chi_square_filepath}/csv-{attr1}-{attr2}.csv", "w") as f:
+        f.write(header)
+        # f.write("\n,")
+        # for x_axis in obj["x_axis"]:
+        #     f.write(f"{x_axis},,")
+        # f.write("\n,")
+
+        # for x_axis in obj["x_axis"]:
+        #     f.write(f"obs,exp,")
+        # f.write("\n")
+
+        f.write("\n")
+        f.write("attribute combination, observed, expected\n")
+        for i in range(row_count):
+            for j in range(column_count):
+                observed = obj["obs"][i][j]
+                expected = obj["expected"][i][j]
+                value_combination = f"{obj["y_axis"][i]} {obj["x_axis"][j]}"
+                value_comb_list.append(value_combination)
+                observed_list.append(observed)
+                expected_list.append(expected)
+                f.write(f"{obj["y_axis"][i]} {obj["x_axis"][j]},{observed},{expected}\n")
+    
+    with open(f"{chi_square_filepath}/csv-exp-obs-{attr1}-{attr2}.csv", "w") as f:
+        f.write(header)
+        f.write("\n,")
+        for x_axis in obj["x_axis"]:
+            f.write(f"{x_axis},,")
+        f.write("\n,")
+
+        for x_axis in obj["x_axis"]:
+            f.write(f"obs,exp,")
+        f.write("\n")
+
+        for i in range(row_count):
+            f.write(f"{obj["y_axis"][i]},")
+            for j in range(column_count):
+                observed = obj["obs"][i][j]
+                expected = obj["expected"][i][j]
+                f.write(f"{observed},{expected},")
+            f.write("\n")
+
+    fig, ax = plt.subplots()
+    plt.xticks(rotation=45, ha="right")
+    ax.tick_params(axis='both', labelsize=5)
+    ax.plot(value_comb_list, observed_list, 'o-', linewidth=1)
+    ax.plot(value_comb_list, expected_list, 'o-', linewidth=1)
+    plt.savefig(f"{figure_path}/plot-{attr1}-{attr2}.png")
+    plt.close()
+                
+
+
 
